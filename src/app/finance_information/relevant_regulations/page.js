8d527fbbab2568296873f89d628ca882bf4957e9
vvitting <140064766+vvitting@users.noreply.github.com>
2026-04-26 17:46:14 +0800

    文字內容更新

--- src/app/finance_information/relevant_regulations/page.js
+++ src/app/finance_information/relevant_regulations/page.js
@@ -13,15 +13,9 @@ export default function AboutContactPage() {
           className="leading-8 text-gray-700 text-justify"
           style={{ fontFamily: "'KaiTi', '標楷體', serif" }}
         >
-          國際：內容請依2026法規與政策Excel檔內之國際法規-名稱(呈現)/生效時間點(呈現，如狀態為Inactive/Repealed，日期後-Inactive 或Repealed)/官方文件(超連結)，如：
+          Coming Soon
         </p>
-        <p
-          className="leading-8 text-gray-700 text-justify mt-4"
-          style={{ fontFamily: "'KaiTi', '標楷體', serif" }}
-        >
-          台灣：內容請依2026法規與政策Excel檔內之台灣法規-名稱(呈現)/生效時間點(呈現，如狀態為Inactive/Repealed，日期後-Inactive 或Repealed)/官方文件(超連結)，如：
-        <a href="https://www.fsc.gov.tw/userfiles/file/1_%E6%B0%B8%E7%BA%8C%E7%B6%93%E6%BF%9F%E6%B4%BB%E5%8B%95%E8%AA%8D%E5%AE%9A%E5%8F%83%E8%80%83%E6%8C%87%E5%BC%95%E7%AC%AC%E4%BA%8C%E7%89%88.pdf">永續經濟活動認定參考指引第二版(20241231)</a>
-        </p>
+        
       </section>
     </main>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -35,13 +35,13 @@ export default function Header() {
     {
       label: "永續財務相關連結",
       items: [
-        { label: "ESG接軌IFRS永續揭露準則", href: "#links-esg-ifrs" },
-        { label: "金融監督管理委員會", href: "https://www.fsc.gov.tw/ch/index.jsp" },
-        { label: "證券櫃檯買賣中心", href: "https://www.tpex.org.tw/zh-tw/index.html" },
-        { label: "台灣證券交易所", href: "https://www.twse.com.tw/zh/index.html" },
-        { label: "台灣期貨交易所", href: "https://www.taifex.com.tw/cht/index" },
-        { label: "台灣期中保管結算所", href: "#links-tdcc" },
-        { label: "財團法人會計研究發展基金會環境部", href: "https://www.ardf.org.tw/" },
+        { label: "ESG接軌IFRS永續揭露準則", href: "#links-esg-ifrs", target: "_blank" },
+        { label: "金融監督管理委員會", href: "https://www.fsc.gov.tw/ch/index.jsp", target: "_blank" },
+        { label: "證券櫃檯買賣中心", href: "https://www.tpex.org.tw/zh-tw/index.html", target: "_blank" },
+        { label: "台灣證券交易所", href: "https://www.twse.com.tw/zh/index.html", target: "_blank" },
+        { label: "台灣期貨交易所", href: "https://www.taifex.com.tw/cht/index", target: "_blank" },
+        { label: "台灣期中保管結算所", href: "#links-tdcc", target: "_blank" },
+        { label: "財團法人會計研究發展基金會環境部", href: "https://www.ardf.org.tw/", target: "_blank" },
       ],
     },
   ];
